5ec742f7fec3e9778631e3e735c4005166a5432d
Luiz Oliveira <oliveira.lfo@gmail.com>
2020-07-17 23:30:17 -0300

    feat: adding morgan

--- auth-api/src/index.ts
+++ auth-api/src/index.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from "express";
 
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
-const PORT = process.env.PORT;
+app.use(morgan('common'));
+
+const PORT = process.env.PORT || 8080;
 
 app.get('/', (req: Request, res: Response) => {
     return res.send({message: "api works fine!"})
