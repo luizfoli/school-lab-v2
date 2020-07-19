654d75308ad40d8b31d93eee8409a9066d5de259
Luiz Oliveira <oliveira.lfo@gmail.com>
2020-07-19 17:10:07 -0300

    feat: Creating Home Component

---
+++ frontend/src/Home/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Home.css';
+
+
+function Home() {
+
+
+  return (
+    <main>
+      <section id="leftMenu">
+      </section>
+      <section id="mainContent">
+      </section>
+    </main>
+  );
+}
+
+export default Home;
--- frontend/src/Subject/Subject.tsx
+++ frontend/src/Subject/Subject.tsx
@@ -3,9 +3,10 @@ import React from 'react';
 function Subject() {
 
     return(
-      <div>
-        <h1>Hi I'm Subject</h1>  
-      </div>
+      <form className="form-container">
+        <label htmlFor="inputSubjectName">Name</label>
+        <input type="text" id="inputSubjectName" name="inputSubjectName"/>
+      </form>
     );
 };
 
