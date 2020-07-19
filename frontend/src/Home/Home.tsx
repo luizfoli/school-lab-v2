a619ebbe8e1f9c912a84e6a5a0e6596e99d56495
Luiz Oliveira <oliveira.lfo@gmail.com>
2020-07-19 17:13:51 -0300

    feat: Creating header component

--- frontend/src/Home/Home.tsx
+++ frontend/src/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Home.css';
 
+import Header from '../MainContent/Header';
 
 function Home() {
 
@@ -10,6 +11,7 @@ function Home() {
       <section id="leftMenu">
       </section>
       <section id="mainContent">
+        <Header />
       </section>
     </main>
   );
